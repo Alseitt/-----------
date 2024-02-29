a1b9f3bd9e74b93ce7b3b9e570835d1be588d812
Alseitt <a_zhauken@kbtu.kz>
2024-02-29 11:03:27 +0600

    Alseitt

--- lab/lab5/regex/4.py
+++ lab/lab5/regex/4.py
@@ -1,4 +1,4 @@
 import re
 
 pattern = re.compile(r"[A-Z]{1}[a-z]+")
-print(pattern.findall("Dulat Sultanbek programming Principles 2"))
+print(pattern.findall("Alseit Zhauken programming Principles 2"))
